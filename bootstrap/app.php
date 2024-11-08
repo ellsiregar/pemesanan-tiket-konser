ec1da6b2336042a6ca37ab685579ba28294fd20d
raffiahmadsp <raphysp65@gmail.com>
2024-11-08 14:05:28 +0700

    memebuat fungsi login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Unauthorized;
+use App\Http\Middleware\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role'  => User::class,
+            'guest'  => Unauthorized::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
