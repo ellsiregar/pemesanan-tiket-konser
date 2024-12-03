60c9d43e55d0ca6ba15ee178c56a60f96a3f3a72
iqblhnf <iqbalhanif30052004@gmail.com>
2024-12-03 11:44:35 +0700

    perbaikan konser di dashboard pengguna

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -45,41 +45,43 @@
         /* Opsional: Huruf besar semua */
     }
 </style>
-<div class="row">
-    @foreach($konsers as $konser)
-    <a href="" style="text-decoration: none;">
+<div class="container-fluid">
+    <div class="row g-4">
+        @foreach($konsers as $konser)
         <div class="col-3">
-            <div class="card shadow">
-                <div class="image-wrapper">
-                    <img src="{{ asset('storage/' . $konser->foto) }}" class="card-img-top" alt="...">
-                    <div class="circle">
-                        <span class="date-text">
-                            {{ strtoupper(\Carbon\Carbon::parse($konser->tanggal_konser)->format('M')) }}<br>
-                            {{ \Carbon\Carbon::parse($konser->tanggal_konser)->format('d-Y') }}
-                        </span>
+            <a href="" style="text-decoration: none;">
+                <div class="card shadow">
+                    <div class="image-wrapper">
+                        <img src="{{ asset('storage/' . $konser->foto) }}" class="card-img-top" alt="...">
+                        <div class="circle">
+                            <span class="date-text">
+                                {{ strtoupper(\Carbon\Carbon::parse($konser->tanggal_konser)->format('M')) }}<br>
+                                {{ \Carbon\Carbon::parse($konser->tanggal_konser)->format('d-Y') }}
+                            </span>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="card-body">
+                        <h5 class="card-title">Concert Linda Coline</h5>
+                        <p class="card-text">{{ Str::limit($konser->deskripsi, 100, '...') }}</p>
+                        <p class="d-flex align-items-center" style="gap: 5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                            </svg>
+                            <small>{{ $konser->lokasi }}</small>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-between">
+                            <small style="font-size: 16px; font-weight: 600;">Get Ticket</small>
+                            <small style="font-size: 16px; font-weight: 600;" class="text-danger">$10 - $30</small>
+                        </div>
                     </div>
                 </div>
-                <br>
-                <div class="card-body">
-                    <h5 class="card-title">Concert Linda Coline</h5>
-                    <p class="card-text">{{ Str::limit($konser->deskripsi, 100, '...') }}</p>
-                    <p class="d-flex align-items-center" style="gap: 5px;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-                            <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                        </svg>
-                        <small>{{ $konser->lokasi }}</small>
-                    </p>
-                </div>
-                <div class="card-footer">
-                    <div class="d-flex justify-content-between">
-                        <small style="font-size: 16px; font-weight: 600;">Get Ticket</small>
-                        <small style="font-size: 16px; font-weight: 600;" class="text-danger">$10 - $30</small>
-                    </div>
-                </div>
-            </div>
+            </a>
         </div>
-    </a>
-    @endforeach
+        @endforeach
+    </div>
 </div>
 @endsection
