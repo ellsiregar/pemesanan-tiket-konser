02793a218b4b6793781eda83e805e3e687d8dfe1
ellsiregar <elisabetsiregar07@gmail.com>
2024-12-06 15:23:04 +0700

    fungsi transaksi

--- app/Http/Controllers/Frontend/TransaksiController.php
+++ app/Http/Controllers/Frontend/TransaksiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Tiket;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
 class TransaksiController extends Controller
@@ -11,6 +12,24 @@ class TransaksiController extends Controller
     public function transaksi() {
         $tikets = Tiket::all();
         return view('frontend.user.transaksi', compact('tikets'));
+    }
+
+    public function chekout(Request $request)
+    {
+
+        $request->validate([
+            'nama' => 'required|string',
+            'email' => 'required|email|max:255',
+            'phone' => 'required|min:10',
+        ]);
+
+        $transaksi = Transaksi::create([
+            'nama'=> $request->nama,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+        return view('', compact('transaksi'));
+
     }
 
 
